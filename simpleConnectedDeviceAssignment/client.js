713574538590f011332c50f65fae1c893d6a3cdb
HenokGuluma <htg218@nyu.edu>
2019-03-05 10:52:11 +0400

    Create client.js

---
+++ simpleConnectedDeviceAssignment/client.js
@@ -0,0 +1,10 @@
+function ledON() {
+    console.log ("sending a message to turn on the LED");
+    
+    // send a packet which will be read by the arduino and assigns variables a value of high.  
+}
+
+function ledOFF() {
+    console.log ("sending a message to turn off the LED");
+    // send a packet which will be read by the arduino and assigns variables a value of low. 
+}
