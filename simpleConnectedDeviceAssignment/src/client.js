0aedadded95234322ef794b6a956e02badd311a8
HenokGuluma <htg218@nyu.edu>
2019-03-14 13:55:43 +0400

    Update client.js

--- simpleConnectedDeviceAssignment/src/client.js
+++ simpleConnectedDeviceAssignment/src/client.js
@@ -1,10 +1,45 @@
-function ledON() {
+function greenledON() {
     console.log ("sending a message to turn on the LED");
+    
+    socket.emit('greenled', this.value);
     
     // send a packet which will be read by the arduino and assigns variables a value of high.  
 }
 
-function ledOFF() {
+function greenledOFF() {
     console.log ("sending a message to turn off the LED");
+    
+    socket.emit('greenled', this.value);
     // send a packet which will be read by the arduino and assigns variables a value of low. 
 }
+//---------------------------------------------------------------------------------------------------
+function blueledON() {
+    console.log ("sending a message to turn on the LED");
+    
+    socket.emit('blueled', this.value);
+    
+    // send a packet which will be read by the arduino and assigns variables a value of high.  
+}
+
+function blueledOFF() {
+    console.log ("sending a message to turn off the LED");
+    
+    socket.emit('blueled', this.value);
+    // send a packet which will be read by the arduino and assigns variables a value of low. 
+}
+//-------------------------------------------------------------------------------------------------------------
+function redledON() {
+    console.log ("sending a message to turn on the LED");
+    
+    socket.emit('redled', this.value);
+    
+    // send a packet which will be read by the arduino and assigns variables a value of high.  
+}
+
+function redledOFF() {
+    console.log ("sending a message to turn off the LED");
+    
+    socket.emit('redled', this.value);
+    // send a packet which will be read by the arduino and assigns variables a value of low. 
+}
+
